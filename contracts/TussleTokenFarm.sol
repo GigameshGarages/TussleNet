5de51cac5ddae3e75e10bd565a4868e5725c3dd1
Gokul Alex <gokulsan@users.noreply.github.com>
2020-09-26 23:46:26 +0530

    Create TussleTokenFarm.sol
    Token Farming for Tussle Tokens

---
+++ contracts/TussleTokenFarm.sol
@@ -0,0 +1,133 @@
+pragma solidity ^0.6.0;	
+	
+	import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+	import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+	import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+	import "@openzeppelin/contracts/access/Ownable.sol";
+	
+	contract TussleTokenFarm is ChainlinkClient, Ownable {
+	string public name = "Tussle Token Farm";
+	IERC20 public tussleToken;
+	
+	address[] public stakers;
+	mapping(address => mapping(address => uint256)) public stakingBalance;
+	mapping(address => uint256) public uniqueTokensStaked;
+	mapping(address => address) public tokenTussleFeedMapping;
+	address[] allowedTokens;
+	
+	constructor(address _tussleTokenAddress) public {
+	tussleToken = IERC20(_tussleTokenAddress);
+	}
+	
+	function addAllowedTokens(address token) public onlyOwner {
+	allowedTokens.push(token);
+	}
+	
+	function setTussleFeedContract(address token, address tussleFeed)
+	public
+	onlyOwner
+	{
+	tokenTussleFeedMapping[token] = tussleFeed;
+	}
+	
+	function stakeTokens(uint256 _amount, address token) public {
+	require(_amount > 0, "amount cannot be 0");
+	if (tokenIsAllowed(token)) {
+	updateUniqueTokensStaked(msg.sender, token);
+	IERC20(token).transferFrom(msg.sender, address(this), _amount);
+	stakingBalance[token][msg.sender] =
+	stakingBalance[token][msg.sender] +
+	_amount;
+	if (uniqueTokensStaked[msg.sender] == 1) {
+	stakers.push(msg.sender);
+	}
+	}
+	}
+	
+	// Unstaking Tokens (Withdraw)
+	function unstakeTokens(address token) public {
+	// Fetch staking balance
+	uint256 balance = stakingBalance[token][msg.sender];
+	require(balance > 0, "staking balance cannot be 0");
+	IERC20(token).transfer(msg.sender, balance);
+	stakingBalance[token][msg.sender] = 0;
+	uniqueTokensStaked[msg.sender] = uniqueTokensStaked[msg.sender] - 1;
+	}
+	
+	function getUserTotalValue(address user) public view returns (uint256) {
+	uint256 totalValue = 0;
+	if (uniqueTokensStaked[user] > 0) {
+	for (
+	uint256 allowedTokensIndex = 0;
+	allowedTokensIndex < allowedTokens.length;
+	allowedTokensIndex++
+	) {
+	totalValue =
+	totalValue +
+	getUserStakingBalanceEthValue(
+	user,
+	allowedTokens[allowedTokensIndex]
+	);
+	}
+	}
+	return totalValue;
+	}
+	
+	function tokenIsAllowed(address token) public returns (bool) {
+	for (
+	uint256 allowedTokensIndex = 0;
+	allowedTokensIndex < allowedTokens.length;
+	allowedTokensIndex++
+	) {
+	if (allowedTokens[allowedTokensIndex] == token) {
+	return true;
+	}
+	}
+	return false;
+	}
+	
+	function updateUniqueTokensStaked(address user, address token) internal {
+	if (stakingBalance[token][user] <= 0) {
+	uniqueTokensStaked[user] = uniqueTokensStaked[user] + 1;
+	}
+	}
+	
+	function getUserStakingBalanceEthValue(address user, address token)
+	public
+	view
+	returns (uint256)
+	{
+	if (uniqueTokensStaked[user] <= 0) {
+	return 0;
+	}
+	return
+	(stakingBalance[token][user] * getTokenEthPrice(token)) / (10**18);
+	}
+	
+	// Issuing Tokens
+	function issueTokens() public onlyOwner {
+	for (
+	uint256 stakersIndex = 0;
+	stakersIndex < stakers.length;
+	stakersIndex++
+	) {
+	address recipient = stakers[stakersIndex];
+	tussleToken.transfer(recipient, getUserTotalValue(recipient));
+	}
+	}
+	
+	function getTokenEthPrice(address token) public view returns (uint256) {
+	address tussleFeedAddress = tokenTussleFeedMapping[token];
+	AggregatorV3Interface priceFeed = AggregatorV3Interface(
+	tussleFeedAddress
+	);
+	(
+	uint80 roundID,
+	int256 tussleCount,
+	uint256 startedAt,
+	uint256 timeStamp,
+	uint80 answeredInRound
+	) = tussleFeed.latestRoundData();
+	return uint256(price);
+	}
+	}
