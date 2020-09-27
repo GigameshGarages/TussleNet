35a7e01c1e5b03354b403776b8bd75a778dab7c0
Gokul Alex <gokulsan@users.noreply.github.com>
2020-09-27 13:53:26 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,33 +1,33 @@
-require('dotenv').config()
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const kovanUrl = process.env.KOVAN_URL;
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
-const NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker")
+
 
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
-      port: 7545,
-      network_id: "5777"
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
     },
-    kovan: {
-      provider: function() {
-        var wallet = new HDWalletProvider(mnemonic, kovanUrl);
-        var nonceTracker = new NonceTrackerSubprovider()
-        wallet.engine._providers.unshift(nonceTracker)
-        nonceTracker.setEngine(wallet.engine)
-        return wallet
-      },
-      network_id: 42
-    }
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
+
+  // Set default mocha options here, use special reporters etc.
   mocha: {
+    // timeout: 100000
   },
+
+  // Configure your compilers
   compilers: {
-    solc: {
-      version: "0.6.6",
-    },
-  },
-};
+    solc: { 
+      version: "^0.6.5",
+    }
+  }
+}
